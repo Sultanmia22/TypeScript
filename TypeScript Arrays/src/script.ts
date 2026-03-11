37f80c7982784e56ce4536299621453132e88917
MD Sultan Mia <sultanmia5732@gmail.com>
2026-03-11 07:35:50 +0600

    TypeScript Arrays

---
+++ TypeScript Arrays/src/script.ts
@@ -0,0 +1,20 @@
+// ** TypeScript Arrays **
+//? TypeScript has a specific syntax for typing arrays.
+
+
+//! Readonly
+//? The readonly keyword can prevent arrays from being changed.
+
+const names : readonly string[] = ['Sultan'];
+
+names.push('Mia'); // Error: Property 'push' does not exist on type 'readonly string[]'.
+
+
+//! Type Inference
+//?TypeScript can infer the type of an array if it has values.
+
+// Example
+const numbers = [1,2,3,4]; // inferred to type number[]
+numbers.push(4)
+
+numbers.push('4'); // Error: Argument of type 'string' is not assignable to parameter of type 'number'.
